a36bbf9f1a9d8616733e73947219bf6b54633a21
Khushi Sinha <100481360+Khushi-83@users.noreply.github.com>
2025-09-16 20:30:22 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 export const Bio = {
   name: "Khushi Sinha",
-  roles: ["Web Applications Developer", "Flutter Developer", "Programmer"],
+  roles: ["Frontend Developer", "Backend Developer", "Problem Solver"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Khushi-83",
